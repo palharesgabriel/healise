be88ea30bdabe374766bffb9276671d2c0a86868
Gabriel Palhares <gaapalhares@gmail.com>
2019-08-19 10:22:30 -0300

    Creating the main Project

---
+++ MyBujo2.0Tests/MyBujo2_0Tests.swift
@@ -0,0 +1,34 @@
+//
+//  MyBujo2_0Tests.swift
+//  MyBujo2.0Tests
+//
+//  Created by Gabriel Palhares on 19/08/19.
+//  Copyright © 2019 Gabriel Palhares. All rights reserved.
+//
+
+import XCTest
+@testable import MyBujo2_0
+
+class MyBujo2_0Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
