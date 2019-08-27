920d7a62ca59272ef82d99cc936480be05d6c02f
adauto <adautopinheiroo@gmail.com>
2019-08-27 11:45:05 -0300

    Remade hierarchy for CalendarView and made all views conform to ViewCode protocol

--- MyBujo2.0/Views/Calendar/CalendarTableViewCell.swift
+++ MyBujo2.0/Views/Calendar/CalendarTableViewCell.swift
@@ -7,21 +7,77 @@
 //
 
 import UIKit
+import JTAppleCalendar
+
+enum CalendarType{
+    case month
+    case week
+    
+    var number: Int {
+        switch self {
+        case .month:
+            return 6
+        default:
+            return 1
+        }
+    }
+}
 
 class CalendarTableViewCell: UITableViewCell, ViewCode {
     
-    var calendarView: CalendarView!
+    
+    let shadowView = ShadowView(frame: .zero)
+    var calendarView: JTACMonthView = JTACMonthView(frame: .zero)
+    var type: CalendarType!
     
     func buildViewHierarchy() {
+        contentView.addSubview(shadowView)
         contentView.addSubview(calendarView)
     }
     
     func setupConstraints() {
-        constraintCalendar()
+        shadowView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            shadowView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            shadowView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            shadowView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16),
+            shadowView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16)
+            ])
+        
+        calendarView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            calendarView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            calendarView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            calendarView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16),
+            calendarView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16)
+            //            calendarView.heightAnchor.constraint(equalToConstant: 168)
+            ])
     }
     
     func setupAdditionalConfigurantion() {
-        //
+        calendarView.backgroundColor = .clear
+        calendarView.scrollingMode = .stopAtEachCalendarFrame
+        calendarView.scrollDirection = .horizontal
+        calendarView.showsHorizontalScrollIndicator = false
+        calendarView.clipsToBounds = true
+        calendarView.layer.cornerRadius = 16
+        calendarView.backgroundColor = .white
+        
+        calendarView.minimumLineSpacing = 0
+        calendarView.minimumInteritemSpacing = 0
+    }
+    
+    func setupCell(calendarType: CalendarType){
+        type = calendarType
+        contentView.backgroundColor = .clear
+        selectionStyle = .none
+        calendarView.register(DayCell.self, forCellWithReuseIdentifier: "dateCell")
+        calendarView.register(MonthHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "monthHeader")
+        calendarView.calendarDelegate = self
+        calendarView.calendarDataSource = self
+        calendarView.scrollToDate(Date(), animateScroll: false)
+        setupView()
+        
     }
     
     
@@ -32,20 +88,13 @@ class CalendarTableViewCell: UITableViewCell, ViewCode {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
-        
-    }
-    
-    func setupCell(calendarType: CalendarType){
-        calendarView = CalendarView(with: calendarType)
         backgroundColor = .clear
-        contentView.backgroundColor = .clear
-        selectionStyle = .none
-        setupView()
+        
     }
     
     func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-        calendarView.viewWillTransition(to: .zero, with: coordinator)
+        let visibleDates = calendarView.visibleDates()
+        calendarView.viewWillTransition(to: .zero, with: coordinator, anchorDate: visibleDates.monthDates.first?.date)
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -58,16 +107,63 @@ class CalendarTableViewCell: UITableViewCell, ViewCode {
 
         // Configure the view for the selected state
     }
-    
-    func constraintCalendar(){
-        calendarView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            calendarView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
-            calendarView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
-            calendarView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16),
-            calendarView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16)
-//            calendarView.heightAnchor.constraint(equalToConstant: 168)
-            ])
-    }
-
 }
+
+extension CalendarTableViewCell: JTACMonthViewDelegate, JTACMonthViewDataSource {
+    func calendar(_ calendar: JTACMonthView, willDisplay cell: JTACDayCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
+        guard let cell = cell as? DayCell else { return }
+        cell.setupCell(cellState: cellState)
+    }
+    
+    func configureCalendar(_ calendar: JTACMonthView) -> ConfigurationParameters {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy MM dd"
+        
+        if type.number == 6{
+            let configParameters = ConfigurationParameters(startDate: dateFormatter.date(from: "\(Calendar.current.component(.year, from: Date())) 01 01")!, endDate: dateFormatter.date(from: "\(Calendar.current.component(.year, from: Date())) 12 31")!, numberOfRows: type.number,calendar: Calendar.current, generateInDates: .forAllMonths, generateOutDates: .tillEndOfRow, firstDayOfWeek: .monday)
+            return configParameters
+        }
+        else{
+            let configParameters = ConfigurationParameters(startDate: dateFormatter.date(from: "\(Calendar.current.component(.year, from: Date())) 01 01")!, endDate: dateFormatter.date(from: "\(Calendar.current.component(.year, from: Date())) 12 31")!, numberOfRows: type.number,calendar: Calendar.current, generateInDates: .forFirstMonthOnly, generateOutDates: .tillEndOfRow, firstDayOfWeek: .sunday)
+            return configParameters
+        }
+        
+    }
+    
+    func calendar(_ calendar: JTACMonthView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTACDayCell {
+        guard let cell = calendarView.dequeueReusableJTAppleCell(withReuseIdentifier: "dateCell", for: indexPath) as? DayCell else { return JTACDayCell()}
+        cell.setupCell(cellState: cellState)
+        return cell
+    }
+    
+    func calendar(_ calendar: JTACMonthView, didSelectDate date: Date, cell: JTACDayCell?, cellState: CellState, indexPath: IndexPath) {
+        guard let cell = cell as? DayCell else { return }
+        cell.setupCell(cellState: cellState)
+    }
+    
+    func calendar(_ calendar: JTACMonthView, didDeselectDate date: Date, cell: JTACDayCell?, cellState: CellState, indexPath: IndexPath) {
+        guard let cell = cell as? DayCell else { return }
+        cell.setupCell(cellState: cellState)
+    }
+    
+    func calendar(_ calendar: JTACMonthView, headerViewForDateRange range: (start: Date, end: Date), at indexPath: IndexPath) -> JTACMonthReusableView {
+        
+        
+        guard let header = calendar.dequeueReusableJTAppleSupplementaryView(withReuseIdentifier: "monthHeader", for: indexPath) as? MonthHeader else { return  JTACMonthReusableView()}
+        header.configureHeader(start: range.start)
+        print(indexPath)
+        return header
+    }
+    
+    func calendarSizeForMonths(_ calendar: JTACMonthView?) -> MonthSize? {
+        return MonthSize(defaultSize: 82)
+    }
+    
+    func calendar(_ calendar: JTACMonthView, shouldSelectDate date: Date, cell: JTACDayCell?, cellState: CellState, indexPath: IndexPath) -> Bool {
+        if cellState.dateBelongsTo == .thisMonth {
+            return true
+        }
+        return false
+    }
+}
+
--- MyBujo2.0/Views/Calendar/MonthHeader.swift
+++ MyBujo2.0/Views/Calendar/MonthHeader.swift
@@ -9,31 +9,23 @@
 import Foundation
 import JTAppleCalendar
 
-class MonthHeader: JTACMonthReusableView {
+class MonthHeader: JTACMonthReusableView, ViewCode {
+    
     var titleLabel = UILabel()
     var weekDaysHeader = WeekDaysView()
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        constraintTitleLabel()
-        constraintWeekDaysHeader()
-    }
     
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    func constraintTitleLabel() {
+    func buildViewHierarchy() {
         addSubview(titleLabel)
+        addSubview(weekDaysHeader)
+    }
+    
+    func setupConstraints() {
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 8),
             titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor)
             ])
-        titleLabel.textColor = UIColor(named:"TitleColor")!
-    }
-    
-    func constraintWeekDaysHeader() {
-        addSubview(weekDaysHeader)
+        
         weekDaysHeader.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             weekDaysHeader.leadingAnchor.constraint(equalTo: leadingAnchor),
@@ -41,6 +33,20 @@ class MonthHeader: JTACMonthReusableView {
             weekDaysHeader.trailingAnchor.constraint(equalTo: trailingAnchor),
             weekDaysHeader.bottomAnchor.constraint(equalTo: bottomAnchor)
             ])
+        
+    }
+    
+    func setupAdditionalConfigurantion() {
+        titleLabel.textColor = UIColor(named:"TitleColor")!
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     func configureHeader(start: Date) {
