ca4cb009fb9f3ac98a5738443aa79963eeccd709
tavares1 <lucastavaresvasconcelos@gmail.com>
2019-08-27 11:01:01 -0300

    GoalsHeaderView structure implemented.

--- MyBujo2.0/Views/MyTodayViews/GoalsViews/GoalsTableViewHeaderView.swift
+++ MyBujo2.0/Views/MyTodayViews/GoalsViews/GoalsTableViewHeaderView.swift
@@ -8,12 +8,54 @@
 
 import UIKit
 
-class GoalsTableViewHeaderView: UITableViewHeaderFooterView {
+class GoalsTableViewHeaderView: UITableViewHeaderFooterView, ViewCode {
+    static let reuseIdentifier = "GoalsTableViewHeaderViewIdentifier"
+    
+    let headerLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.text = "Goals"
+        lbl.font = UIFont(name: "AvenirNext-Medium", size: 28)
+        lbl.textColor = UIColor(named: "TitleColor")!
+        return lbl
+    }()
+    
+    let buttonAddGoal: UIButton = {
+        let btn = UIButton()
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.setTitleColor(UIColor(named: "TitleColor")!, for: .normal)
+        btn.setTitle("+", for: .normal)
+        btn.titleLabel?.font = UIFont(name: "AvenirNext-Medium", size: 28)
+        return btn
+    }()
+    
     override init(reuseIdentifier: String?) {
         super.init(reuseIdentifier: reuseIdentifier)
+        setupView()
     }
     
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder: _) has not been implemented")
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func buildViewHierarchy() {
+        contentView.addSubview(headerLabel)
+        contentView.addSubview(buttonAddGoal)
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            headerLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 32),
+            headerLabel.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            buttonAddGoal.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -32),
+            buttonAddGoal.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+    
+    func setupAdditionalConfigurantion() {
+        backgroundView = UIView()
     }
 }
