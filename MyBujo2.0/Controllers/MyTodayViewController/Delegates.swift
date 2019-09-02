c56deb347a1fd3cc8d6343f2a00dd2172be46f51
adauto <adautopinheiroo@gmail.com>
2019-09-02 08:51:45 -0300

    created folder for MyTodayVC

---
+++ MyBujo2.0/Controllers/MyTodayViewController/Delegates.swift
@@ -0,0 +1,59 @@
+//
+//  Delegates.swift
+//  MyBujo2.0
+//
+//  Created by Adauto Pinheiro on 02/09/19.
+//  Copyright © 2019 Gabriel Palhares. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension MyTodayViewController:TableViewHeaderViewDelegate {
+    func addGoal() {
+        guard let customSplitViewController = splitViewController as? CustomSplitViewController else { return }
+        let viewCont = customSplitViewController.controllers[1]
+        viewCont.definesPresentationContext = true
+        
+        let newGoalViewController = NewGoalViewController()
+        newGoalViewController.day = day
+        newGoalViewController.delegate = self
+        newGoalViewController.modalPresentationStyle = .overCurrentContext
+        viewCont.present(newGoalViewController, animated: true, completion: nil)
+    }
+}
+
+extension MyTodayViewController: MediaCollectionViewDelegate {
+    func pushViewController(viewController: UIViewController) {
+        self.present(viewController, animated: true, completion: nil)
+    }
+}
+
+extension MyTodayViewController: CalendarTableViewCellDelegate {
+    
+    /// Solve Later
+    func didSelectDate(date: Date) {
+        //do something
+        
+        let result = CoreDataManager.fetch(entityClass: Day.self,predicate: EntityType.day(date).predicate)
+        guard let day = result?.first as? Day else {
+            self.day = Day(context: CoreDataManager.context)
+            guard let dateIgnoringTime = date.ignoringTime() else { return }
+            self.day.date = dateIgnoringTime
+            self.day.save()
+            return
+        }
+        self.day = day
+        
+    }
+}
+
+extension MyTodayViewController: NewGoalViewControllerDelegate{
+    func didDismissWithDescript() {
+        tableView.reloadData()
+    }
+    
+    func didDismissWithoutDescript() {
+        //
+    }
+}
--- MyBujo2.0/Controllers/MyTodayViewController/MyTodayViewController.swift
+++ MyBujo2.0/Controllers/MyTodayViewController/MyTodayViewController.swift
@@ -134,51 +134,6 @@ extension MyTodayViewController: UITableViewDelegate, UITableViewDataSource {
     
 }
 
-extension MyTodayViewController:TableViewHeaderViewDelegate {
-    func addGoal() {
-        guard let customSplitViewController = splitViewController as? CustomSplitViewController else { return }
-        let viewCont = customSplitViewController.controllers[1]
-        viewCont.definesPresentationContext = true
-        
-        let newGoalViewController = NewGoalViewController()
-        newGoalViewController.day = day
-        newGoalViewController.delegate = self
-        newGoalViewController.modalPresentationStyle = .overCurrentContext
-        viewCont.present(newGoalViewController, animated: true, completion: nil)
-    }
-}
 
-extension MyTodayViewController: MediaCollectionViewDelegate {
-    func pushViewController(viewController: UIViewController) {
-        self.present(viewController, animated: true, completion: nil)
-    }
-}
 
-extension MyTodayViewController: CalendarTableViewCellDelegate {
-    
-    /// Solve Later
-    func didSelectDate(date: Date) {
-        //do something
 
-        let result = CoreDataManager.fetch(entityClass: Day.self,predicate: EntityType.day(date).predicate)
-        guard let day = result?.first as? Day else {
-            self.day = Day(context: CoreDataManager.context)
-            guard let dateIgnoringTime = date.ignoringTime() else { return }
-            self.day.date = dateIgnoringTime
-            self.day.save()
-            return
-        }
-        self.day = day
-        
-    }
-}
-
-extension MyTodayViewController: NewGoalViewControllerDelegate{
-    func didDismissWithDescript() {
-        tableView.reloadData()
-    }
-    
-    func didDismissWithoutDescript() {
-        //
-    }
-}
