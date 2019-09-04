d5caa06d40f52cec268ee1ff2f3e958b8750a7c8
Rennan <rennanrbs@gmail.com>
2019-09-04 11:45:23 -0300

    extensions

--- MyBujo2.0/Controllers/MasterViewController.swift
+++ MyBujo2.0/Controllers/MasterViewController.swift
@@ -20,28 +20,6 @@ extension UILabel {
     }
 }
 
-extension UIButton {
-    convenience init(title: String) {
-        self.init()
-        self.translatesAutoresizingMaskIntoConstraints = false
-        self.setTitle(title, for: .normal)
-        self.contentHorizontalAlignment = .left
-    }
-    
-    func setBackgroundColor(color: UIColor, forState: UIControl.State) {
-        self.clipsToBounds = true
-        UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
-        if let context = UIGraphicsGetCurrentContext() {
-            context.setFillColor(color.cgColor)
-            context.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
-            let colorImage = UIGraphicsGetImageFromCurrentImageContext()
-            UIGraphicsEndImageContext()
-            self.setBackgroundImage(colorImage, for: forState)
-        }
-    }
-}
-
-
 class MasterViewController: UIViewController, ViewCode {
     
     let monthLabel = UILabel(text: "Lucas,", font: "Avenir Next", fontSize: 60, textColor: UIColor(named:"StartColor")!)
