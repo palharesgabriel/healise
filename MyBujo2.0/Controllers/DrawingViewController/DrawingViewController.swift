1e0f94b934d4e11cd9d3ba33790ad747a5bcc3b2
adauto <adautopinheiroo@gmail.com>
2019-09-10 08:35:40 -0300

    created new folder and new controller

---
+++ MyBujo2.0/Controllers/DrawingViewController/DrawingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DrawingViewController.swift
+//  MyBujo2.0
+//
+//  Created by Adauto Pinheiro on 10/09/19.
+//  Copyright © 2019 Gabriel Palhares. All rights reserved.
+//
+
+import UIKit
+
+class DrawingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
