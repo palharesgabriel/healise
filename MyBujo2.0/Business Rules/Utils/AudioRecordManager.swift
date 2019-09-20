5465578917f399e0eb28451268c54c50a1154016
Gabriel <gaapalhares@gmail.com>
2019-09-20 13:33:00 -0300

    adding AudiRecordManager file

---
+++ MyBujo2.0/Business Rules/Utils/AudioRecordManager.swift
@@ -0,0 +1,13 @@
+//
+//  AudioRecordManager.swift
+//  MyBujo2.0
+//
+//  Created by Gabriel Palhares on 20/09/19.
+//  Copyright © 2019 Gabriel Palhares. All rights reserved.
+//
+
+import AVFoundation
+
+class AudioRecordManager {
+    
+}
