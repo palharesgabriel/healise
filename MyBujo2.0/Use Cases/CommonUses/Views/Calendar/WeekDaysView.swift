c82015ebb83e5769a14df6c31225afb02393653a
Rennan <rennanrbs@gmail.com>
2019-10-08 09:34:47 -0300

    translate calendar

--- MyBujo2.0/Use Cases/CommonUses/Views/Calendar/WeekDaysView.swift
+++ MyBujo2.0/Use Cases/CommonUses/Views/Calendar/WeekDaysView.swift
@@ -8,21 +8,37 @@
 
 import UIKit
 
+extension UILabel {
+    convenience init(dayName: String) {
+        self.init()
+        text = NSLocalizedString(dayName, comment: dayName)
+    }
+}
+
 class WeekDaysView: UIStackView {
     
     
     // MARK: Properties
     let daysNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
     
+    lazy var daysLabels: [UILabel] = {
+        var labels = [UILabel]()
+        self.daysNames.forEach { name in
+            labels.append(UILabel(dayName: name))
+        }
+        return labels
+    }()
+    
+    
     // MARK: Initialization
     override init(frame: CGRect) {
         super.init(frame: frame)
         distribution = .fillEqually
-        for index in daysNames {
+        for index in daysLabels {
             let label = UILabel()
             label.font = UIFont(name: "AvenirNext-Medium", size: 16)
             label.textAlignment = .center
-            label.text = index
+            label.text = index.text
             label.textColor = UIColor(named:"TitleColor")!
             self.addArrangedSubview(label)
         }
