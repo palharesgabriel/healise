b4195de655b837836f3b3ba502b1bdf9d87fda12
adauto <adautopinheiroo@gmail.com>
2019-10-23 09:42:51 -0300

    [NEW] Arrows on calendar header

--- MyBujo2.0/Use Cases/CommonUses/Views/Calendar/MonthHeader.swift
+++ MyBujo2.0/Use Cases/CommonUses/Views/Calendar/MonthHeader.swift
@@ -6,19 +6,54 @@
 //  Copyright © 2019 Gabriel Palhares. All rights reserved.
 //
 
-import Foundation
+import UIKit
 import JTAppleCalendar
 
 class MonthHeader: JTACMonthReusableView, ViewCode {
     
     
     // MARK: Properties
+    var delegate: CalendarHeaderDelegate?
     var titleLabel = UILabel()
     var weekDaysHeader = WeekDaysView()
+    
+    lazy var arrowRight: UIButton = {
+        let imageView = UIButton()
+        var image = UIImage()
+        if #available(iOS 13.0, *) {
+            let configuration = UIImage.SymbolConfiguration(pointSize: 24)
+            image = (UIImage(systemName: "arrowtriangle.right.fill", withConfiguration: configuration)?.withRenderingMode(.alwaysTemplate))!
+        } else {
+            // Fallback on earlier versions
+        }
+        imageView.setImage(image, for: .normal)
+        imageView.tintColor = UIColor(named: "ActionColor")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.addTarget(self, action: #selector(didTapRightButton), for: .touchUpInside)
+        return imageView
+    }()
+    
+   lazy var arrowLeft: UIButton = {
+        let imageView = UIButton()
+        var image = UIImage()
+        if #available(iOS 13.0, *) {
+            let configuration = UIImage.SymbolConfiguration(pointSize: 24)
+            image = (UIImage(systemName: "arrowtriangle.left.fill", withConfiguration: configuration)?.withRenderingMode(.alwaysTemplate))!
+        } else {
+            // Fallback on earlier versions
+        }
+        imageView.setImage(image, for: .normal)
+        imageView.tintColor = UIColor(named: "ActionColor")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.addTarget(self, action: #selector(didTapLeftButton), for: .touchUpInside)
+        return imageView
+    }()
     
     func buildViewHierarchy() {
         addSubview(titleLabel)
         addSubview(weekDaysHeader)
+        addSubview(arrowRight)
+        addSubview(arrowLeft)
     }
     
     // MARK: Initialized
@@ -28,6 +63,14 @@ class MonthHeader: JTACMonthReusableView, ViewCode {
     }
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc func didTapLeftButton(){
+        delegate?.didTapLeftButton()
+    }
+    
+    @objc func didTapRightButton(){
+        delegate?.didTapRightButton()
     }
     
     
@@ -43,9 +86,16 @@ class MonthHeader: JTACMonthReusableView, ViewCode {
             weekDaysHeader.leadingAnchor.constraint(equalTo: leadingAnchor),
             weekDaysHeader.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
             weekDaysHeader.trailingAnchor.constraint(equalTo: trailingAnchor),
-            weekDaysHeader.bottomAnchor.constraint(equalTo: bottomAnchor)
+            weekDaysHeader.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            arrowRight.centerXAnchor.constraint(equalTo: centerXAnchor, constant: (frame.width/4)),
+            arrowRight.centerYAnchor.constraint(equalTo: titleLabel.centerYAnchor, constant: 0),
+            
+            arrowLeft.centerXAnchor.constraint(equalTo: centerXAnchor, constant: -(frame.width/4)),
+            arrowLeft.centerYAnchor.constraint(equalTo: titleLabel.centerYAnchor, constant: 0)
             ])
     }
+    
     func setupAdditionalConfigurantion() {
         titleLabel.textColor = UIColor(named:"TitleColor")!
     }
