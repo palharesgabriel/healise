cf2dc167b31fb34b0961d7aace5abb7215153f15
Rennan <rennanrbs@gmail.com>
2019-11-22 20:53:17 -0300

    [new]: remove warning extension uicolor

--- MyBujo2.0/Business Rules/Extension/UIColor+Extension.swift
+++ MyBujo2.0/Business Rules/Extension/UIColor+Extension.swift
@@ -9,9 +9,9 @@
 import UIKit
 
 extension UIColor {
-    static let titleColor = UIColor(named: "TitleColor")!
-    static let cardsColor = UIColor(named: "CardsColor")!
-    static let selectionColor = UIColor(named: "SelectionColor")!
-    static let actionColor = UIColor(named: "ActionColor")!
-    static let blueBackground = UIColor(named: "BlueBackground")!
+    static let titleColor = UIColor(named: "TitleColor") ?? UIColor.black
+    static let cardsColor = UIColor(named: "CardsColor") ?? UIColor.white
+    static let selectionColor = UIColor(named: "SelectionColor") ?? UIColor.purple
+    static let actionColor = UIColor(named: "ActionColor") ?? UIColor.blue
+    static let blueBackground = UIColor(named: "BlueBackground") ?? UIColor.lightGray
 }
